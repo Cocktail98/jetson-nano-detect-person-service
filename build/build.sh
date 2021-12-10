cf5847475b3b5d138333693cd1421ec95681907b
GuangchenJ <guangchen@mail.ynu.edu.cn>
2021-12-10 20:24:16 +0800

    update Multithreading Operation

--- build/build.sh
+++ build/build.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-#printf "make clean ..."
-#make clean
-#printf "\n make clean finished ..."
+printf "[INFO] make clean ..."
+make clean
+printf "\n[INFO] make clean finished ..."
 
-printf "\n Generate gRPC ..."
+printf "\n[INFO] Generate gRPC ..."
 cd ..
 sh gen_grpc.sh
 # shellcheck disable=SC2164
 cd build
-printf "\n Generate gRPC finished ..."
+printf "\n[INFO] Generate gRPC finished ..."
 
 cmake -DCMAKE_BUILD_TYPE=Release ..
 
-printf "\nUsing %d cores to build ...\n\n" "$(nproc)"
+printf "\n[INFO] Using %d cores to build ...\n\n" "$(nproc)"
 
 cmake --build . -j"$(nproc)"
-printf "\nBuild finished ...\n\n"
+printf "\n[INFO] Build finished ...\n\n"
--- gen_grpc.sh
+++ gen_grpc.sh
@@ -3,7 +3,7 @@
 grpc_cpp_plugin_location=$(which grpc_cpp_plugin)
 
 protoc --proto_path=./proto/ --grpc_out=./detect_service --plugin=protoc-gen-grpc="$grpc_cpp_plugin_location" detect.proto
-printf "Generate 'detect.proto' message classes finished\n"
+printf "[INFO] Generate 'detect.proto' message classes finished\n"
 
 protoc --proto_path=./proto/ --cpp_out=./detect_service detect.proto
-printf "Generate 'detect.proto' service classes finished\n"
+printf "[INFO] Generate 'detect.proto' service classes finished\n"
